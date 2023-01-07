b3ab41956030e64a04a6ebbe6c44b59234eafe67
Ori2846 <hdo2846@gmail.com>
2023-01-06 17:20:20 -0800

    CSS update

--- mainButInFlaskForm.py
+++ mainButInFlaskForm.py
@@ -9,21 +9,11 @@ def home():
 
 @app.route('/download', methods=['POST'])
 def download():
-    # get the YouTube URL from the form submission
     url = request.form['url']
-
-    # create a YouTube object using the URL
     yt = YouTube(url)
-
-    # get the first video in the list of available videos
     video = yt.streams.filter(res='1080p', file_extension='mp4').first()
-
-    # save the video to a local file
     video.download('./videos')
-    # get the file name of the downloaded video
     file_name = video.default_filename
-
-    # send the video file as a response to the request
     return send_from_directory('./videos', file_name, as_attachment=True)
 
 if __name__ == "__main__":
